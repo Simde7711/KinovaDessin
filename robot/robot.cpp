3b4ce458903af928af34f9579640d528c78426ed
Simon Descôteaux <simde771@gmail.com>
2025-04-15 21:11:53 -0400

    Ajout du .h et .cpp de Robot

---
+++ robot/robot.cpp
@@ -0,0 +1,6 @@
+#include "robot.h"
+// #include JSON HEADER ET AUTRE LIBRAIRIES
+
+Robot::Robot(){}
+
+Robot::~Robot(){}
---
+++ robot/robot.h
@@ -0,0 +1,9 @@
+class Robot
+{
+    public:
+        Robot();
+        ~Robot();
+
+    private:
+
+};
