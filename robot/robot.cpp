6de321a86a7a23355db243276b686c0eb11fe058
Simon Descôteaux <simde771@gmail.com>
2025-04-25 22:22:39 -0400

    POINTER TO YOUR MOM'S ADDRESS GRAHHHHHH

--- robot/robot.cpp
+++ robot/robot.cpp
@@ -1,11 +1,11 @@
 #include "robot.h"
 // #include JSON HEADER ET AUTRE LIBRAIRIES
 
-Robot::Robot(std::string& _ipRobot, Commands& _commands, Parameters& _parameters)
+Robot::Robot(std::string *_ipRobot, Commands *_commands, Parameters *_parameters)
 {
-    ipRobot = _ipRobot;
-    commandsPtr = new Commands(_commands);
-    parametersPtr = new Parameters(_parameters);
+    ipRobot = *_ipRobot;
+    commandsPtr = new Commands(*_commands);
+    parametersPtr = new Parameters(*_parameters);
 }
 
 Robot::~Robot()
--- robot/robot.h
+++ robot/robot.h
@@ -6,13 +6,13 @@
 class Robot
 {
     public:
-        Robot(std::string& _ipRobot, Commands& _commands, Parameters& _parameters);
+        Robot(std::string *_ipRobot, Commands *_commands, Parameters *_parameters);
         ~Robot();
 
         std::string ipRobot;
 
     private:
-        Commands* commandsPtr;
-        Parameters* parametersPtr;
+        Commands *commandsPtr;
+        Parameters *parametersPtr;
 
 };
