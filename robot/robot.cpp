c79becb4a3ce0d0a117920b39a70b22c3b28336f
Simon Descôteaux <simde771@gmail.com>
2025-04-30 19:19:54 -0400

    Continuation de la classe

--- robot/robot.cpp
+++ robot/robot.cpp
@@ -1,6 +1,8 @@
 #include "robot.h"
+
+// std
 #include <thread>
-// #include JSON HEADER ET AUTRE LIBRAIRIES
+#include <iostream>
 
 Robot::Robot(std::string *_ipRobot, Commands *_commands, Parameters *_parameters)
 {
@@ -18,6 +20,45 @@ Robot::~Robot()
     delete parametersPtr;
 }
 
+void Robot::CreateAPI()
+{
+    auto error_callback = [](k_api::KError err){ cout << "_________ callback error _________" << err.toString(); };
+    transport = new k_api::TransportClientTcp();
+    RTtransport = new k_api::TransportClientUdp();
+    router = new k_api::RouterClient(transport, error_callback);
+    RTrouter = new k_api::RouterClient(RTtransport, error_callback);
+    transport->connect(ipRobot, PORT);
+    RTtransport->connect(ipRobot, PORT_REAL_TIME);
+    // Set session data connection information
+    auto create_session_info = k_api::Session::CreateSessionInfo();
+    create_session_info.set_username("DessinKinova");
+    create_session_info.set_password("MOT_DE_PASSE");
+    create_session_info.set_session_inactivity_timeout(60000);   // (milliseconds)
+    create_session_info.set_connection_inactivity_timeout(2000); // (milliseconds)
+
+    // Session manager service wrapper
+    std::cout << "Creating session for communication" << std::endl;
+    session_manager = new k_api::SessionManager(router);
+    session_manager->CreateSession(create_session_info);
+    RTsession_manager = new k_api::SessionManager(RTrouter);
+    RTsession_manager->CreateSession(create_session_info);
+    
+    std::cout << "Session created" << std::endl;
+
+    // Create DeviceConfigClient and BaseClient
+    device_config = new k_api::DeviceConfig::DeviceConfigClient(router);
+    basecyclic = new k_api::BaseCyclic::BaseCyclicClient(RTrouter);
+    base = new k_api::Base::BaseClient(router);
+}
+
+void Robot::Prepare()
+{
+    auto servomode = k_api::Base::ServoingModeInformation();
+	servomode.set_servoing_mode(k_api::Base::ServoingMode::LOW_LEVEL_SERVOING);
+    base->SetServoingMode(servomode);
+	// std::this_thread::sleep_for(std::chrono::milliseconds(500));
+}
+
 void Robot::UpdateParameters(Parameters *_parameters)
 {
     *parametersPtr = *_parameters;
@@ -25,5 +66,8 @@ void Robot::UpdateParameters(Parameters *_parameters)
 
 void Robot::Process()
 {
+    CreateAPI();
+    Prepare();
 
+    // ici le while
 }
--- robot/robot.h
+++ robot/robot.h
@@ -1,7 +1,20 @@
 #include "structJson.h"
 
+// kinovaAPI
+#include <SessionManager.h>
+#include <DeviceConfigClientRpc.h>
+#include <BaseClientRpc.h>
+#include <TransportClientUdp.h>
+#include <TransportClientTcp.h>
+#include <BaseCyclicClientRpc.h>
+#include <BaseClientRpc.h>
+#include <RouterClient.h>
+
+// std
 #include <string>
 #include <vector>
+
+namespace k_api = Kinova::Api;
 
 class Robot
 {
@@ -19,4 +32,18 @@ class Robot
 
         void Process();
 
+        // API kinova
+        void CreateAPI();
+        void Prepare();
+        #define PORT 10000
+        #define PORT_REAL_TIME 10001
+        k_api::Base::BaseClient *base;
+        k_api::BaseCyclic::BaseCyclicClient *basecyclic;
+        k_api::DeviceConfig::DeviceConfigClient *device_config;
+        k_api::SessionManager *session_manager;
+        k_api::SessionManager *RTsession_manager;
+        k_api::RouterClient *router;
+        k_api::RouterClient *RTrouter;
+        k_api::TransportClientTcp *transport;
+        k_api::TransportClientUdp *RTtransport;
 };
